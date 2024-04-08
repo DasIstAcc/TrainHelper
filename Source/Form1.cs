c589d2a77a15717c79a157b7bcd4258b3662ce42
DasIstAcc <55249088+DasIstAcc@users.noreply.github.com>
2024-04-08 15:12:09 +0300

    Doing cool stuff

--- Source/Form1.cs
+++ Source/Form1.cs
@@ -16,5 +16,10 @@ namespace TrainHelper
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wow that was really cool!");
+        }
     }
 }
